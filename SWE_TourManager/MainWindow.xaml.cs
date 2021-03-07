3ef3f09db94ffce413e1248ed412a14fce686525
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-03-07 16:24:29 +0100

    btn

--- SWE_TourManager/MainWindow.xaml.cs
+++ SWE_TourManager/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,47 @@ namespace SWE_TourManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        int clicked = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void buttonHello_Click(object sender, RoutedEventArgs e)
+        {
+            
+            switch (clicked)
+            {
+                case 0:
+                    buttonHello.Content = "Why";
+                    break;
+                case 1:
+                    buttonHello.Content = "Stop";
+                    break;
+                case 3:
+                    buttonHello.Width = 200;
+                    buttonHello.Content = "Seriously, Stop";
+                    break;
+                case 5:
+                    buttonHello.Content = "(╯°□°）╯︵ ┻━┻";
+                    break;
+                case 6:
+                    buttonHello.Width = 400;
+                    buttonHello.Content = "At least you are not double clicking...";                    
+                    break;
+            }
+
+            clicked++;
+        }
+
+        private void buttonHello_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            buttonHello.Background = Brushes.DarkCyan;
+            buttonHello.Width = 200;
+            buttonHello.Content = "now you broke me :(";
+            buttonHello.IsEnabled = false;
         }
     }
 }
