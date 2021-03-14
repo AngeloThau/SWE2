65868517bb451eccbbaa4f8ccc25952ed5e41615
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-03-14 16:33:25 +0100

    View von Mainwindow angepasst

---
+++ SWE_TourManager/Models/MainWindowModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE_TourManager.Models
+{
+    class MainWindowModel
+    {
+
+        public void btn_click()
+        {
+            //here is code
+        }
+
+    }
+}
---
+++ SWE_TourManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Threading.Tasks;
+
+namespace SWE_TourManager.ViewModels
+{
+    class MainWindowViewModel 
+    {
+
+        private ICommand textboxUpdate;
+        private ICommand button;
+
+        private class Command : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Execute(object parameter)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
