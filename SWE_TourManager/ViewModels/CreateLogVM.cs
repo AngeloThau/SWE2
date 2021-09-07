1ae6309a5dacb38aae39b8b1e08bc33d8bd94a47
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-09-07 16:54:43 +0200

    Creating Logs
    Jetzt gehts wirklich

--- SWE_TourManager/ViewModels/CreateLogVM.cs
+++ SWE_TourManager/ViewModels/CreateLogVM.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using SWE_TourManager.ViewModels.Commands;
 using SWE_TourManager.BusinessLayer;
+using System.Windows;
 
 namespace SWE_TourManager.ViewModels
 {
@@ -14,21 +15,26 @@ namespace SWE_TourManager.ViewModels
     {
         private ITourManagerFactory tourManagerFactory;
         private TourItem currentTour;
-        private string NewLogName;
-        private double NewLogDistance;
-        private int NewLogTime;
-        private int NewLogRating;
-        private int NewLogSpeed;
-        private int NewLogVerUp;
-        private int NewLogVerDown;
-        private int NewLogDiff;
-        private DateTime NewLogDate;
-        private string NewLogReport;
-
-        private ICommand cancelCommand;       
+        private string newLogName;
+        private double newLogDistance;
+        private int newLogTime;
+        private int newLogRating;
+        private int newLogSpeed;
+        private int newLogVerUp;
+        private int newLogVerDown;
+        private int newLogDiff;
+        private DateTime newLogDate = DateTime.Now;
+        private string newLogReport;
+      
         private ICommand createLogCommand;
         public ICommand CreateLogCommand => createLogCommand ??= new RelayCommand(CreateLog);
-        public ICommand CancelCommand => cancelCommand ??= new RelayCommand(Cancel);
+
+        public CreateLogVM(TourItem tour)
+        {
+            this.currentTour = tour;
+            this.tourManagerFactory = TourManagerFactory.GetInstance();
+    }
+
         public TourItem CurrentTour
         {
             get
@@ -44,19 +50,189 @@ namespace SWE_TourManager.ViewModels
                 }
             }
         }
-        public CreateLogVM(TourItem tour)
+        public DateTime NewLogDate
         {
-            CurrentTour = tour;
+            get
+            {
+                return newLogDate;
+            }
+            set
+            {
+                if (newLogDate != value)
+                {
+                    newLogDate = value;
+                    RaisePropertyChangedEvent(nameof(NewLogDate));
+                }
+            }
+        }
+
+        public string NewLogName
+        {
+            get
+            {
+                return newLogName;
+            }
+            set
+            {
+                if (newLogName != value)
+                {
+                    newLogName = value;
+                    RaisePropertyChangedEvent(nameof(NewLogName));
+                }
+            }
+        }
+
+        public double NewLogDistance
+        {
+            get
+            {
+                return newLogDistance;
+            }
+            set
+            {
+                if (newLogDistance != value)
+                {
+                    newLogDistance = value;
+                    RaisePropertyChangedEvent(nameof(NewLogDistance));
+                }
+            }
+        }
+        public int NewLogTime
+        {
+            get
+            {
+                return newLogTime;
+            }
+            set
+            {
+                if (newLogTime != value)
+                {
+                    newLogTime = value;
+                    RaisePropertyChangedEvent(nameof(NewLogTime));
+                }
+            }
+        }
+
+        public int NewLogRating
+        {
+            get
+            {
+                return newLogRating;
+            }
+            set
+            {
+                if (newLogRating != value)
+                {
+                    newLogRating = value;
+                    RaisePropertyChangedEvent(nameof(NewLogRating));
+                }
+            }
+        }
+
+        public int NewLogSpeed
+        {
+            get
+            {
+                return newLogSpeed;
+            }
+            set
+            {
+                if (newLogSpeed != value)
+                {
+                    newLogSpeed = value;
+                    RaisePropertyChangedEvent(nameof(NewLogSpeed));
+                }
+            }
+        }
+
+        public int NewLogVerUp
+        {
+            get
+            {
+                return newLogVerUp;
+            }
+            set
+            {
+                if (newLogVerUp != value)
+                {
+                    newLogVerUp = value;
+                    RaisePropertyChangedEvent(nameof(NewLogVerUp));
+                }
+            }
+        }
+
+        public int NewLogDiff
+        {
+            get
+            {
+                return newLogDiff;
+            }
+            set
+            {
+                if (newLogDiff != value)
+                {
+                    newLogDiff = value;
+                    RaisePropertyChangedEvent(nameof(NewLogDiff));
+                }
+            }
+        }
+        public int NewLogVerDown
+        {
+            get
+            {
+                return newLogVerDown;
+            }
+            set
+            {
+                if (newLogVerDown != value)
+                {
+                    newLogVerDown = value;
+                    RaisePropertyChangedEvent(nameof(NewLogVerDown));
+                }
+            }
+        }
+
+        public string NewLogReport
+        {
+            get
+            {
+                return newLogReport;
+            }
+            set
+            {
+                if (newLogReport != value)
+                {
+                    newLogReport = value;
+                    RaisePropertyChangedEvent(nameof(NewLogReport));
+                }
+            }
         }
 
        
-
         private void CreateLog(object commandParameter)
-        {
-            LogItem generatedLog = tourManagerFactory.CreateLog(CurrentTour, NewLogName, NewLogDistance, NewLogTime, NewLogRating, NewLogSpeed, NewLogVerUp, NewLogVerDown, NewLogDiff, NewLogDate, NewLogReport );
-            
+        {           
+            if (NewLogName == null || NewLogReport == null || NewLogName == "" || NewLogReport == "")
+            {
+                MessageBox.Show("Parameter Missing, Report and Name Cannot be empty");
+                return;
+            }
+            LogItem generatedLog = tourManagerFactory.CreateLog(CurrentTour, NewLogName, NewLogDistance, NewLogTime, NewLogRating, NewLogSpeed, NewLogVerUp, NewLogVerDown, NewLogDiff, NewLogDate, NewLogReport);
+            MessageBox.Show("Successfully created a new Log with Name " + NewLogName);
+            ClearLogWindow();
         }
 
-
+        public void ClearLogWindow()
+        {
+            NewLogDistance = 0;
+            NewLogTime = 0;
+            NewLogName = "";
+            NewLogReport = "";
+            NewLogRating = 1;
+            NewLogDate = DateTime.Now;
+            NewLogDiff = 1;
+            NewLogSpeed = 0;
+            NewLogVerDown = 0;
+            NewLogVerUp = 0;           
+        }
     }
 }
