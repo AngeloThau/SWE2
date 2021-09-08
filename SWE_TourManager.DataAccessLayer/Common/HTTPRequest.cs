b1ce0455f78677b3780a37ee46684564681a033c
if19b100@technikum-wien-at <if19b100@technikum-wien.at>
2021-09-08 15:38:07 +0200

    HTTP requester (DAL)
    Im DAL meinen HTTP Requester gebaut, fehlt noch implementierung durch BL und Model
    Außerdem Test-Projekt hinzugefügt und config geändert (Filepaths und HTTP connection strings hinzugefügt)

---
+++ SWE_TourManager.DataAccessLayer/Common/HTTPRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE_TourManager.DataAccessLayer.Common
+{
+    public class HTTPRequest : IHTTPRequest
+    {
+
+        private string url;
+        private string response;
+
+        public HTTPRequest()
+        {
+            url = ConfigurationManager.AppSettings["DataUrl"].ToString(); 
+        }
+        public string GetJsonResponse(string Start, string Destination)
+        {
+            Uri requestUri = RequestBuilder(Start, Destination);
+            SendRequest(requestUri);
+            return response;
+        }
+
+        public Uri RequestBuilder(string Start, string Destination)
+        {
+            string requesturi = url + "&from=" + Start + "&to=" + Destination;
+
+            return new Uri(requesturi);
+        }
+
+        public void SendRequest(Uri Request)
+        {
+            WebRequest request = WebRequest.Create(Request);
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream);
+                this.response = reader.ReadToEnd();
+            }
+            response.Close();
+
+        }
+    }
+}
